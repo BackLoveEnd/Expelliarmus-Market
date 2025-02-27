5382ee596f29693158350e22431414ddc66c8531
Igarevv <naturelovee12@gmail.com>
2025-02-27 22:16:59 +0200

    change migrations

--- backend/modules/User/routes/api.php
+++ backend/modules/User/routes/api.php
@@ -3,18 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Http\Controllers\PasswordController;
 use Laravel\Fortify\Http\Controllers\ProfileInformationController;
+use Modules\User\Http\Controllers\GuestsController;
 use Modules\User\Http\Controllers\UserController;
-
-/*
- *--------------------------------------------------------------------------
- * API Routes
- *--------------------------------------------------------------------------
- *
- * Here is where you can register API routes for your application. These
- * routes are loaded by the RouteServiceProvider within a group which
- * is assigned the "api" middleware group. Enjoy building your API!
- *
-*/
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/current-user', [UserController::class, 'user'])->middleware('auth:sanctum');
@@ -26,4 +16,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::put('/user/password', [PasswordController::class, 'update'])
         ->middleware([config('fortify.auth_middleware', 'auth').':'.config('fortify.guard')])
         ->name('user-password.update');
+});
+
+Route::prefix('management/users')->group(function () {
+    Route::get('/regular-customers', [UserController::class, 'getRegularCustomers']);
+
+    Route::get('/guests', [GuestsController::class, 'getGuests']);
 });
