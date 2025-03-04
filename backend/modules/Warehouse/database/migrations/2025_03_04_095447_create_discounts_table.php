aef1b8b63722c1cecda174633b5cdc9885e22e74
Igarevv <naturelovee12@gmail.com>
2025-03-04 18:18:19 +0200

    discount migrations

---
+++ backend/modules/Warehouse/database/migrations/2025_03_04_095447_create_discounts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('discounts', function (Blueprint $table) {
+            $table->unsignedBigInteger('id')->generatedAs()->always();
+            $table->tinyInteger('percentage');
+            $table->decimal('original_price');
+            $table->decimal('discount_price');
+            $table->timestamp('start_date');
+            $table->timestamp('end_date');
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('discounts');
+    }
+};
---
+++ backend/modules/Warehouse/database/migrations/2025_03_04_095529_create_product_combined_variation_discounts_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_combined_variation_discounts', function (Blueprint $table) {
+            $table->foreignId('c_variation_id')->constrained('product_variations')->cascadeOnDelete();
+            $table->foreignId('discount_id')->constrained()->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_combined_variation_discounts');
+    }
+};
