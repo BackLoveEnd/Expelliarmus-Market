61cd880d9fa7a31dbc9669c22432d61718cd5a9d
Igarevv <forstudy757@gmail.com>
2025-03-14 17:57:42 +0200

    discount factory

--- backend/modules/Warehouse/database/seeders/WarehouseDatabaseSeeder.php
+++ backend/modules/Warehouse/database/seeders/WarehouseDatabaseSeeder.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Seeder;
 
 class WarehouseDatabaseSeeder extends Seeder
 {
+
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
+        $this->call([
+            DiscountedProductsSeeder::class,
+        ]);
     }
+
 }
