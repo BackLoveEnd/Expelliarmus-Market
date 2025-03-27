1bf7c6a66d23205a2619bdd5aa9d2808309fd021
Igarevv <naturelovee12@gmail.com>
2025-03-27 14:17:57 +0200

    docker/servers update

---
+++ backend/modules/Product/app/Http/Shop/Actions/GetRelatedToProductProductsAction.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Product\Http\Shop\Actions;
+
+use Modules\Category\Models\Category;
+
+class GetRelatedToProductProductsAction
+{
+    public function handle(Category $category) {}
+}
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,43 +1,43 @@
 import axios from "axios";
-import { useAuthStore } from "@/stores/useAuthStore.js";
+import {useAuthStore} from "@/stores/useAuthStore.js";
 
-export default function api(isApiCall = true) {
-  const api = axios.create({
-    baseURL: `http://api.expelliarmus.com:8080${isApiCall ? "/api" : ""}`,
-    withCredentials: true,
-    withXSRFToken: true,
-    xsrfCookieName: "XSRF-TOKEN",
-    xsrfHeaderName: "X-XSRF-TOKEN",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  api.interceptors.request.use(async (config) => {
-    if (config.method !== "get" && !config.headers["X-XSRF-TOKEN"]) {
-      await axios.get("http://api.expelliarmus.com:8080/sanctum/csrf-cookie", {
+export default function api() {
+    const api = axios.create({
+        baseURL: `/api`,
         withCredentials: true,
-      });
-    }
+        withXSRFToken: true,
+        xsrfCookieName: "XSRF-TOKEN",
+        xsrfHeaderName: "X-XSRF-TOKEN",
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
 
-    return config;
-  });
+    api.interceptors.request.use(async (config) => {
+        if (config.method !== "get" && !config.headers["X-XSRF-TOKEN"]) {
+            await axios.get(`/api/sanctum/csrf-cookie`, {
+                withCredentials: true,
+            });
+        }
 
-  api.interceptors.response.use(
-    function (response) {
-      return response;
-    },
-    function (error) {
-      if (error.request?.status === 403) {
-      }
+        return config;
+    });
 
-      if ([401, 419].includes(error.request?.status)) {
-        useAuthStore().forgetUser();
-      }
+    api.interceptors.response.use(
+        function (response) {
+            return response;
+        },
+        function (error) {
+            if (error.request?.status === 403) {
+            }
 
-      return Promise.reject(error);
-    },
-  );
+            if ([401, 419].includes(error.request?.status)) {
+                useAuthStore().forgetUser();
+            }
 
-  return api;
+            return Promise.reject(error);
+        },
+    );
+
+    return api;
 }
