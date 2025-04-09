aadc31c2b5c56e59239a24e7dc1305bab803cde8
Igarevv <naturelovee12@gmail.com>
2025-04-09 16:42:57 +0300

    min max products prices by brand

--- backend/modules/Product/app/Http/Shop/Controllers/MinMaxPricesProductController.php
+++ backend/modules/Product/app/Http/Shop/Controllers/MinMaxPricesProductController.php
@@ -6,7 +6,9 @@ namespace Modules\Product\Http\Shop\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\JsonResponse;
+use Modules\Brand\Models\Brand;
 use Modules\Category\Models\Category;
+use Modules\Product\Http\Shop\Actions\GetMinMaxProductPriceForBrandAction as PriceBrandAction;
 use Modules\Product\Http\Shop\Actions\GetMinMaxProductsPriceAction;
 use Modules\Product\Http\Shop\Actions\GetMinMaxProductsPriceForCategoryAction as PriceCategoryAction;
 
@@ -47,6 +49,30 @@ class MinMaxPricesProductController extends Controller
     public function getMinMaxProductsPriceForCategory(Category $category, PriceCategoryAction $action): JsonResponse
     {
         $prices = $action->handle($category);
+
+        return response()->json([
+            'data' => [
+                'type' => 'staff',
+                'attributes' => [
+                    'min_price' => $prices->min_price ?? 0,
+                    'max_price' => $prices->max_price ?? 0,
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * Retrieve min and max prices of products for a specific brand.
+     *
+     * Usage place - Shop.
+     *
+     * @param  Brand  $brand
+     * @param  PriceBrandAction  $action
+     * @return JsonResponse
+     */
+    public function getMinMaxProductsPriceForBrand(Brand $brand, PriceBrandAction $action): JsonResponse
+    {
+        $prices = $action->handle($brand);
 
         return response()->json([
             'data' => [
