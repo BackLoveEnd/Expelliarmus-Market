a039044be46b5d26a2c97247de80b70f11bf4d7b
Igarevv <naturelovee12@gmail.com>
2025-04-09 17:47:34 +0300

    categories for brands backend

---
+++ backend/modules/Category/app/Http/Shop/Actions/GetCategoriesForBrandAction.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Category\Http\Shop\Actions;
+
+use Illuminate\Database\Eloquent\Collection;
+use Modules\Brand\Models\Brand;
+use Modules\Warehouse\Enums\ProductStatusEnum;
+
+class GetCategoriesForBrandAction
+{
+    public function handle(string|int $brandId): Collection
+    {
+        $brand = Brand::query()
+            ->when(
+                value: is_numeric($brandId),
+                callback: fn($query) => $query->where('id', $brandId),
+                default: fn($query) => $query->where('slug', $brandId),
+            )
+            ->firstOrFail();
+
+        return $brand
+            ->categories()
+            ->where('products.status', ProductStatusEnum::PUBLISHED)
+            ->orderBy('categories.name')
+            ->get(['categories.id', 'categories.name', 'categories.slug']);
+    }
+}
---
+++ backend/modules/Category/app/Http/Shop/Controllers/CategoriesBrandController.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Category\Http\Shop\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Modules\Category\Http\Shop\Actions\GetCategoriesForBrandAction as BrandCategoriesAction;
+use Modules\Category\Http\Shop\Resources\CategoriesBrowseListResource;
+use TiMacDonald\JsonApi\JsonApiResourceCollection;
+
+class CategoriesBrandController extends Controller
+{
+    /**
+     * Get the categories for a specific brand.
+     *
+     * Usage place - Shop.
+     *
+     * @param  string|int  $brandId
+     * @param  BrandCategoriesAction  $action
+     * @return JsonApiResourceCollection|JsonResponse
+     */
+    public function getCategoriesForBrand(
+        string|int $brandId,
+        BrandCategoriesAction $action,
+    ): JsonApiResourceCollection|JsonResponse {
+        $categories = $action->handle($brandId);
+
+        if ($categories->isEmpty()) {
+            return response()->json(['message' => 'Categories not found.'], 404);
+        }
+
+        return CategoriesBrowseListResource::collection($categories);
+    }
+}
