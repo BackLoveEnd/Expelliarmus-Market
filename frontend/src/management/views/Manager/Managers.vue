55448876bea2926ce746d8f1c0ff53364b01f1ba
Igarevv <naturelovee12@gmail.com>
2025-04-11 13:24:19 +0300

    manager register form

---
+++ frontend/src/management/views/Manager/Managers.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import DefaultContainer from "@/management/components/Main/DefaultContainer.vue";
+import RegisterManager from "@/management/components/Managers/RegisterManager.vue";
+</script>
+
+<template>
+  <default-container>
+    <div class="space-y-16 my-14">
+      <section class="container mx-auto flex flex-col">
+        <div>
+          <h1 class="font-semibold text-4xl">Managers</h1>
+        </div>
+      </section>
+      <section class="container mx-auto">
+        <register-manager/>
+      </section>
+    </div>
+  </default-container>
+</template>
+
+<style scoped>
+
+</style>
