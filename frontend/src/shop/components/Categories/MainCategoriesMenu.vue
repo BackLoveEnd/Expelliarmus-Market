e6ffadb079fbe0fff6bbaeeb12a73a9057b84c26
kulakov <alexkharchuk2004@gmail.com>
2025-04-14 12:05:54 +0000

    some fixes megamenu

--- frontend/src/shop/components/Categories/MainCategoriesMenu.vue
+++ frontend/src/shop/components/Categories/MainCategoriesMenu.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from 'vue'
 import { CategoryService } from '@/services/CategoryService.js'
 import { MegaMenu } from 'primevue'
+import 'primeicons/primeicons.css'
 
 const items = ref([])
 
@@ -34,13 +35,19 @@ const transformCategories = (categories) => {
 </script>
 
 <template>
-  <MegaMenu :model="items" orientation="vertical" scroll-height="25rem">
-
+  <MegaMenu
+      :model="items"
+      orientation="vertical"
+      scroll-height="25rem"
+      class="max-h-[25rem]"
+  >
     <template #item="{ item }">
       <router-link v-slot="{ href, navigate }" :to="item.route" custom>
         <a :href="href" @click="navigate">
           <span :class="item.icon"/>
-          <span class="ml-2">{{ item.label }}</span>
+          <span class="ml-2 text-lg">{{ item.label }}</span>
+          <i v-if="Array.isArray(item.items) && item.items.length > 0"
+             class="pi pi-angle-right text-gray-600 text-base"></i>
         </a>
       </router-link>
     </template>
