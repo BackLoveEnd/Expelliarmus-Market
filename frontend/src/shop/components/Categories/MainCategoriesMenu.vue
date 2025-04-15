b186e7d33c9066c81b3350e4e02fcad5cd281391
kulakov <alexkharchuk2004@gmail.com>
2025-04-15 11:48:47 +0000

    test

--- frontend/src/shop/components/Categories/MainCategoriesMenu.vue
+++ frontend/src/shop/components/Categories/MainCategoriesMenu.vue
@@ -1,17 +1,16 @@
 <script setup>
-import {onMounted, ref} from "vue";
-import {CategoryService} from "@/services/Category/CategoryService.js";
-import {MegaMenu} from "primevue";
+import { onMounted, ref } from 'vue'
+import { CategoryService } from '@/services/Category/CategoryService.js'
+import TieredMenu from 'primevue/tieredmenu'
 
-
-const items = ref([]);
+const items = ref([])
 
 onMounted(async () => {
   await CategoryService.getCategoriesTree()
       .then((response) => {
-        items.value = transformCategories(response?.data.data);
-      });
-});
+        items.value = transformCategories(response?.data.data)
+      })
+})
 
 const transformCategories = (categories) => {
   return categories.map(category => ({
@@ -29,24 +28,51 @@ const transformCategories = (categories) => {
               : []
         }))]
         : []
-  }));
-};
+  }))
+}
 
 </script>
 
 <template>
-  <MegaMenu :model="items" orientation="vertical">
+  <div class="max-h-[400px] overflow-y-auto w-64 border rounded">
+    <TieredMenu :model="items" class="w-full">
+      <template #item="{ item }">
+        <router-link
+            v-if="item.route"
+            v-slot="{ href, navigate }"
+            :to="item.route"
+            custom
+        >
+          <a
+              :href="href"
+              @click="navigate"
+              class="flex items-center justify-between px-4 py-2 hover:bg-gray-100 transition"
+          >
+            <div class="flex items-center">
+              <span v-if="item.icon" :class="item.icon"/>
+              <span class="ml-2">{{ item.label }}</span>
+            </div>
+            <i
+                v-if="Array.isArray(item.items) && item.items.length > 0"
+                class="pi pi-angle-right text-sm text-gray-600"
+            ></i>
+          </a>
+        </router-link>
 
-    <template #item="{ item }">
-      <router-link v-slot="{ href, navigate }" :to="item.route" custom>
-        <a :href="href" @click="navigate">
-          <span :class="item.icon"/>
-          <span class="ml-2">{{ item.label }}</span>
+        <a
+            v-else
+            class="flex items-center justify-between px-4 py-2 hover:bg-gray-100 transition"
+        >
+          <div class="flex items-center">
+            <span v-if="item.icon" :class="item.icon"/>
+            <span class="ml-2">{{ item.label }}</span>
+          </div>
         </a>
-      </router-link>
-    </template>
-  </MegaMenu>
+      </template>
+    </TieredMenu>
+  </div>
 </template>
+
 
 <style scoped>
 
